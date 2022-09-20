31fa48f38eeb09f476fd941d7a648e8f351f97b0
jeongeum <wjddma1202@naver.com>
2022-09-21 03:28:37 +0900

    [add] js 파일 추가

--- 1만시간의법칙/js/10000hours.js
+++ 1만시간의법칙/js/10000hours.js
@@ -1,1 +1,10 @@
 // js file
+
+const goBtn = document.querySelector(".btn-go");
+const modal = document.querySelector(".cont-modal");
+const endBtn = document.querySelector("btn-end");
+const modalClass = modal.classList.contains("ir");
+
+goBtn.addEventListener("click", function () {
+  modal.classList.remove("ir");
+});
