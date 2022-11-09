3a08c78874aee1860825b01ce83e2ba337f3c337
Jeongeum <wjddma1202@naver.com>
2022-11-10 03:20:02 +0900

    1109 React , Node, Linux

---
+++ Node/003.js
@@ -0,0 +1,5 @@
+const 이름 = "호준";
+const 나이 = 10;
+
+module.exports.이름 = 이름; // 0. 모듈 생략 가능
+module.exports.나이 = 나이;
---
+++ Node/005/003.js
@@ -0,0 +1,8 @@
+// 신버전
+// export : 밖에서도 해당 변수를 사용할 수 있게 하는 문법
+export const 이름 = "호준";
+export const 나이 = 10;
+
+// 구버전
+// module.exports.이름 = 이름; // 0. 모듈 생략 가능
+// module.exports.나이 = 나이;
